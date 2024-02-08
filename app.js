ac166f73c9b4ed3bd386b68186b7697b8962e401
Jatasya Raval <66833319+JatasyaR@users.noreply.github.com>
2024-02-08 22:17:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const element = document.getElementById("no");
 
-    // Attach click event listener to the element
     element.addEventListener("click", () => {
 
         element.style.left = `${Math.floor(Math.random() * 100) - 70}vw`;
